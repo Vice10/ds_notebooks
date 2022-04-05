0c59bc6c537a0b2f998f41664f5d9a0c62841885
Vice10 <42076380+Vice10@users.noreply.github.com>
2022-04-05 12:27:19 +0300

    Add files via upload

---
+++ whr_src.r
@@ -0,0 +1,76 @@
+WHR_Data = read.csv("whr_2021.csv")
+head(WHR_Data, 6)
+happyData <- WHR_Data[,c(2:8)]
+
+library(PerformanceAnalytics)
+
+# HEATPLOT
+library(ggplot2)
+library(reshape2)
+cormat<-round(cor(happyData),2)
+melted_cormat<-melt(cormat)
+
+# Get lower triangle of the correlation matrix
+get_lower_tri<-function(cormat){
+  cormat[upper.tri(cormat)] <- NA
+  return(cormat)
+}
+# Get upper triangle of the correlation matrix
+get_upper_tri <- function(cormat){
+  cormat[lower.tri(cormat)]<- NA
+  return(cormat)
+}
+
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()
+
+reorder_cormat <- function(cormat){
+  # Use correlation between variables as distance
+  dd <- as.dist((1-cormat)/2)
+  hc <- hclust(dd)
+  cormat <-cormat[hc$order, hc$order]
+}
+
+
+cormat <- reorder_cormat(cormat)
+upper_tri <- get_upper_tri(cormat)
+# Melt the correlation matrix
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+# Create a ggheatmap
+ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  coord_fixed()
+ggheatmap + 
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.5, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+
+
+# CORR MATRICES
+corrplot.mixed(cor(happyData, method = "spearman"), p.mat = res1$p, insig = "p-value", tl.col="black")
+
+corrplot.mixed(cor(happyData, method = "kendall"), p.mat = res1$p, insig = "p-value", tl.col="black")
+
+corrplot.mixed(cor(happyData), p.mat = res1$p, insig = "p-value", tl.col="black")
+
+print(res1$p)
+
+# PEARSON CC TEST
+cor.test(happyData$Ladder, happyData$SS, method = "pearson")
+
